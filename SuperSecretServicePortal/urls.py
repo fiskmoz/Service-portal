e5c0c21435ae2436ce26f5dbd86ead2b98383954
Christoffer Sundqvist <fiskmoz96@gmail.com>
2018-11-14 10:27:14 +0100

    Finished login/logout support + homepage

--- SuperSecretServicePortal/urls.py
+++ SuperSecretServicePortal/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('django.contrib.auth.urls')),
+    path('auth', include('django.contrib.auth.urls')),
     path('form', include('form.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
 ]
