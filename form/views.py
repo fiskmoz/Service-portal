80a6b53ae309699eaa6191aaa35e26a86573b5a5
Christoffer Sundqvist <fiskmoz96@gmail.com>
2018-11-14 11:28:25 +0100

    Added .gitignore ?

--- form/views.py
+++ form/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 # Create your views here.
-def mainpage(request):
-    return HttpResponse("You are in forms.")
+def NewForm(request):
+    return HttpResponse("HEJ")
