9fec1e93027a370c55c898260277638e73990ec7
Christoffer Sundqvist <fiskmoz96@gmail.com>
2018-11-22 13:26:42 +0100

    Fixed parameters for editing order ID's

--- forms/urls.py
+++ forms/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('Create', views.NewForm, name='newform'),
-    path('Edit', views.EditForm, name='editform'),
-    path('^(?P<order_id>)/$', views.OrderDetail, name = 'orderdetail'),
+    path('<order_id>/edit', views.EditForm, name='editform'),
+    path('<order_id>', views.OrderDetail, name = 'orderdetail'),
     path('', views.ViewForms, name='viewform')
 ]
