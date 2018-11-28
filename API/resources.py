9164a41bfac99a9cb524f80e10a3f9d142c3a2f8
håkan <hokanw@hotmail.com>
2018-11-28 11:27:37 +0100

    added filter to order API

--- API/resources.py
+++ API/resources.py
@@ -1,7 +1,13 @@
 from tastypie.resources import ModelResource
 from API.models import Order
+from tastypie.authorization import Authorization
 
 class OrderResource(ModelResource):
     class Meta:
         queryset = Order.objects.all()
         resource_name = 'order'
+        allowed_methods = ['get','post']
+        authorization = Authorization()
+
+    def get_object_list(self, request):
+        return super(OrderResource, self).get_object_list(request).filter(User=request.user)
