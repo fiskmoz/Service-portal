020a65b0e3df57e05ffb06f773f2086f6856362c
Joakim Djuvfeldt <jocke150@hotmail.com>
2018-11-29 13:07:55 +0100

    Linked new DB model

--- API/admin.py
+++ API/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Order
+from .models import Resources
 
 # Register your models here.
 admin.site.register(Order)
+admin.site.register(Resources)
