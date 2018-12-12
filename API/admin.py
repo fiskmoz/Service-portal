59a91e37dd9a276aa5bfd71cab270c69afbbca39
Joakim Djuvfeldt <jocke150@hotmail.com>
2018-12-12 11:32:30 +0100

    New tables

--- API/admin.py
+++ API/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Order
-from .models import Resources
+from .models import Resources, SystemIdentif, NewResource
 
 # Register your models here.
 admin.site.register(Order)
 admin.site.register(Resources)
+admin.site.register(SystemIdentif)
+admin.site.register(NewResource)
