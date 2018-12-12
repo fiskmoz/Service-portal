4824be3d9a89dc93768ab1455097138e52f0bff7
Joakim Djuvfeldt <jocke150@hotmail.com>
2018-12-12 13:17:49 +0100

    Modified db tables

--- API/admin.py
+++ API/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from .models import Order
-from .models import Resources, SystemIdentif, NewResource
+from .models import SystemIdentif, NewResource
 
 # Register your models here.
 admin.site.register(Order)
-admin.site.register(Resources)
 admin.site.register(SystemIdentif)
 admin.site.register(NewResource)
