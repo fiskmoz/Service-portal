b0a467a1c54a699cee65cc09d3f87ae94da53242
Joakim Djuvfeldt <jocke150@hotmail.com>
2018-12-13 10:48:00 +0100

    Added Serializer

--- API/serializer.py
+++ API/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Order
 from .models import NewResource
-from .models import SystemIdentif
+from .models import SystemIdentif, CompleteOrder
 
 class OrderSerializer(serializers.ModelSerializer):
 
@@ -20,6 +20,11 @@ class SystemIdentifSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SystemIdentif
+        fields = ('__all__')
+
+class CompleteOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompleteOrder
         fields = ('__all__')
 
 # Converts to JSON
