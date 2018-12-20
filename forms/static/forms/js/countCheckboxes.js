5a75aac7a7e2d202e59644bdc738507af986be56
Marcus Viebke <marcvieb100@student.kau.se>
2018-12-20 09:06:04 +0100

    Fixed checkbox functionality and minor refactoring

--- forms/static/forms/js/countCheckboxes.js
+++ forms/static/forms/js/countCheckboxes.js
@@ -1,26 +1,26 @@
 function countCheckboxes(Medal){
-  var inputElems = document.getElementsByTagName("input"),
-    count = 0;
+  var inputElems = document.getElementsByTagName("input"), count = 0;
     for(var i=0; i<inputElems.length; i++) {
       if(inputElems[i].type === "checkbox" && inputElems[i].checked === true){
         count++;
       }
     }
-    //Do something with Medal
-
-    if(count >= 25 && Medal == 'Gold'){
+    var amountOfCheckboxes = inputElems.length;
+    var goldLimit = amountOfCheckboxes / 2;
+    var silverLimit = amountOfCheckboxes / 5;
+    if(count >= goldLimit && Medal == 'Gold'){
       alert("Order sent");
       document.contract.submit();
     }
-    if(count >= 25 && Medal != 'Gold')
+    if(count >= goldLimit && Medal != 'Gold')
       alert("It is recommended you have chosen Service Tier Gold");
-    if(count < 25 && count > 10 && Medal == 'Silver'){
+    if(count < goldLimit && count > silverLimit && Medal == 'Silver'){
       alert("Order sent");
       document.contract.submit();
     }
-    if(count < 25 && count > 10 && Medal != 'Silver')
+    if(count < goldLimit && count > silverLimit && Medal != 'Silver')
       alert("It is recommended you have chosen Service Tier Silver");
-    if(count <= 10 && Medal == 'Bronze'){
+    if(count <= silverLimit && Medal == 'Bronze'){
       alert("Order sent");
       document.contract.submit();
     }
